a4e6a2f44a3e1cf41ba7e5cdc4603173d8e2dd40
Greg Bayer <greg@gbayer.com>
2011-04-23 18:33:05 -0700

    Added support for passing OAuth params via POST or Authorization header. Cleaned up logging.

--- src/authentication.py
+++ src/authentication.py
@@ -34,25 +34,34 @@ class TwoLeggedOAuthAuthentication(object):
         values in "Authorization" header, or as a GET request
         or in a POST body.
         """
-        logging.info("TwoLeggedOAuthAuthentication request: " + repr(request))
-        
-        auth_header = {}
-        if 'HTTP_AUTHORIZATION' in request.META:
-            auth_header = {'Authorization':request.META.get('HTTP_AUTHORIZATION')}
+        logging.info("TwoLeggedOAuthAuthentication")
 
-        oauth_server, oauth_request = initialize_server_request(request)
+        oauth_server, oauth_request = initialize_oauth_server_request(request)
         try:
-            oauth_server.verify_request(oauth_request, 
-                get_consumer(request.GET.get('oauth_consumer_key')),
-                None)
+            key = request.GET.get('oauth_consumer_key')
+            if not key:
+                key = request.POST.get('oauth_consumer_key')
+            if not key:
+                auth_header_value = request.META.get('HTTP_AUTHORIZATION')
+                key = get_oauth_consumer_key_from_header(auth_header_value)
+            if not key:
+                logging.error('TwoLeggedOAuthAuthentication. No consumer_key found.')
+                return None
+            # Raises exception if it doesn't pass 
+            oauth_server.verify_request(oauth_request, get_consumer(key), None)
+            # If OAuth authentication is successful, set oauth_consumer_key on request in case we need it later 
+            request.META['oauth_consumer_key'] = key
             return True
-        except oauth2.Error:
+        except oauth2.Error, e:
             logging.exception("Error in TwoLeggedOAuthAuthentication.")
             request.user = AnonymousUser()
             return False
-        except KeyError:
+        except KeyError, e:
             logging.exception("Error in TwoLeggedOAuthAuthentication.")
             request.user = AnonymousUser()
+            return False
+        except Exception, e:
+            logging.exception("Error in TwoLeggedOAuthAuthentication.")
             return False
 
         return True
@@ -62,6 +71,57 @@ class TwoLeggedOAuthAuthentication(object):
         resp['WWW-Authenticate'] = "Token Based Authentication"
         resp.status_code = 401
         return resp
+
+
+def initialize_oauth_server_request(request):
+    """
+    OAuth initialization.
+    """
+    
+    # Since 'Authorization' header comes through as 'HTTP_AUTHORIZATION', convert it back
+    auth_header = {}
+    if 'HTTP_AUTHORIZATION' in request.META:
+        auth_header = {'Authorization':request.META.get('HTTP_AUTHORIZATION')}
+    
+    absolute_uri = request.build_absolute_uri()
+    url = absolute_uri
+    if absolute_uri.find('?') != -1:
+        url = absolute_uri[:absolute_uri.find('?')]
+        
+    oauth_request = oauth2.Request.from_request(
+            request.method, url, headers=auth_header, 
+            parameters=dict(request.REQUEST.items()))
+        
+    if oauth_request:
+        oauth_server = oauth2.Server(signature_methods={
+            # Supported signature methods
+            'HMAC-SHA1': oauth2.SignatureMethod_HMAC_SHA1()
+        })
+
+    else:
+        oauth_server = None
+        
+    return oauth_server, oauth_request
+
+
+def get_oauth_consumer_key_from_header(auth_header_value):
+    key = None
+    
+    # Process Auth Header
+    # Check that the authorization header is OAuth.
+    if not auth_header_value:
+        return None
+    if auth_header_value[:6] == 'OAuth ':
+        auth_header = auth_header_value[6:]
+        try:
+            # Get the parameters from the header.
+            header_params = oauth2.Request._split_header(auth_header)
+            if 'oauth_consumer_key' in header_params:
+                key = header_params['oauth_consumer_key']
+        except:
+            raise Error('Unable to parse OAuth parameters from '
+                'Authorization header.')
+    return key
 
 
 def get_consumer(oauth_consumer_key):
@@ -78,29 +138,4 @@ def lookup_consumer(key):
         return consumer
     except Consumer.DoesNotExist:
         return None
-
-
-def initialize_server_request(request):
-    """
-    Shortcut for initialization.
-    """
-    absolute_uri = request.build_absolute_uri()
-    if absolute_uri.find('?') != -1:
-        url = absolute_uri[:absolute_uri.find('?')]
-    logging.info("url: " + url)
-    oauth_request = oauth2.Request.from_request(
-            request.method, url, headers=request.META, 
-            parameters=dict(request.REQUEST.items()))
-        
-    if oauth_request:
-        oauth_server = oauth2.Server(signature_methods={
-            # Supported signature methods
-            'HMAC-SHA1': oauth2.SignatureMethod_HMAC_SHA1()
-        })
-
-    else:
-        oauth_server = None
-        
-    return oauth_server, oauth_request
-
 
